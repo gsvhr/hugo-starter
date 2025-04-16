d60acc258fb23acfccfc868a4f268e776f148f89
Степан <79278993666@ya.ru>
2025-04-16 22:31:40 +0400

    refactoring

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,5 +1,23 @@
-import { initForm } from './contact';
+import { initContactForm } from './contact';
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll<HTMLFormElement>('.contact-form').forEach(initForm);
+  // Проверка сохраненной темы
+  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+  // Инициализация контактных форм
+  document.querySelectorAll<HTMLFormElement>('.contact-form').forEach(initContactForm);
+  // Инициализация переключателей темы
+  document.querySelectorAll('.theme-switcher button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const isDark = document.documentElement.classList.toggle('dark');
+      localStorage.setItem('theme', isDark ? 'dark' : 'light');
+    });
+  });
+  // Проверка поддержки CSS  браузером
+  if (typeof CSSLayerBlockRule == 'undefined') {
+    alert('Ваш браузер устарел. 😢\nСайт отображается не корректно.\nОбновите версию браузера.');
+  }
 });
