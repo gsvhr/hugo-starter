07930bdf593e18835792e0b5215968b53fc567db
Степан <79278993666@ya.ru>
2025-04-18 00:17:58 +0400

    refactoring

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,23 +1,24 @@
 import { initContactForm } from './contact';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Проверка сохраненной темы
-  if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
   // Инициализация контактных форм
   document.querySelectorAll<HTMLFormElement>('.contact-form').forEach(initContactForm);
+
   // Инициализация переключателей темы
-  document.querySelectorAll('.theme-switcher button').forEach((button) => {
-    button.addEventListener('click', () => {
-      const isDark = document.documentElement.classList.toggle('dark');
-      localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  document.querySelectorAll<HTMLInputElement>('.theme-controller').forEach((checkbox) => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    checkbox.checked = savedTheme === 'dark';
+    document.documentElement.dataset.theme = savedTheme;
+    checkbox.addEventListener('change', (e) => {
+      const target = e.target as HTMLInputElement;
+      const theme = target.checked ? 'dark' : 'light';
+      localStorage.setItem('theme', theme);
+      document.documentElement.dataset.theme = theme;
     });
   });
+
   // Проверка поддержки CSS  браузером
   if (typeof CSSLayerBlockRule == 'undefined') {
-    //alert('Ваш браузер устарел. 😢\nСайт отображается не корректно.\nОбновите версию браузера.');
+    alert('Ваш браузер устарел. 😢\nСайт отображается не корректно.\nОбновите версию браузера.');
   }
 });
